d0f28ac8732ab90b91901fc051363589323fbc22
JH51 <jonhuang5@gmail.com>
2022-12-13 11:56:26 -0500

    Changed default port of app server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 require("dotenv").config();
 
 const MODE = process.env.NODE_ENV || "production";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9990;
 
 app.use(express.json());
 
