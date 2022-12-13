cac113260b08ab5c15518faf542f355e9c806a68
JH51 <jonhuang5@gmail.com>
2022-12-13 11:50:37 -0500

    Set up dummy application with simple Express for discovering, monitoring endpoints

---
+++ dummy/server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+const fetch = require("node-fetch");
+// const responseTime = require("response-time");
+// const { startMetricsServer, restResponseTimeHistogram } = require("./metrics.js");
+
+// // Inject response time; response time will send metrics to Histogram
+// app.use(responseTime((req, res, time) => {
+//   if (req.url) {
+//     console.log('time:',time);
+//     restResponseTimeHistogram.observe({
+//       method: req.method,
+//       route: req.url,
+//       status_code: res.statusCode
+//     }, time / 1000)
+//   }
+// }));
+
+app.use(express.json());
+app.use(require("api-express-exporter")()); // That's it!
+
+app.get("/fast", (req, res) => {
+  res.status(200).send("fast");
+});
+
+app.get("/slow", (req, res) => {
+  setTimeout(() => res.status(200).send("fast"), 1000);
+});
+
+app.use("/good", (req, res) => {
+  return res.sendStatus(202);
+});
+
+app.use("/bad", (req, res) => {
+  return res.sendStatus(505);
+});
+
+app.use("/error", (req, res) => {
+  try {
+    throw new Error("something broke...");
+  } catch (error) {
+    res.status(506).send(error);
+  }
+});
+
+app.get(
+  "/someotherroute",
+  (req, res, next) => {
+    fetch("https://google.com");
+    // .then((fetchResponse) => fetchResponse.text())
+    // .then((responseText) => console.log(responseText));
+    return next();
+  },
+  (req, res) => {
+    res.send();
+  }
+);
+
+app.use("/", (req, res) => res.send("HELLO WORLD"));
+
+app.listen(PORT, () => {
+  console.log(`Express server started on port ${PORT}`);
+  // startMetricsServer();
+});
