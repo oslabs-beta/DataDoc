bb9f27fd2338e94ccd713843cd0a4bcdbea08194
JH51 <jonhuang5@gmail.com>
2022-12-13 00:29:50 -0500

    Reconfigured webpack; set publicPath to /; provided more default environment variables

--- client/index.js
+++ client/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-// import { render } from "react-dom";
-// import App from "./components/App.jsx";
+import App from "./components/App.jsx";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-    <>
-    Text from index.js
-    {/* <App /> */}
-    </>
-)
+  <>
+    <App />
+  </>
+);
