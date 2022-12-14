53ca11a3291de4a2d9b55084a93e4e01b039c2c7
Jamie Schiff <jamie.abrams.schiff@gmail.com>
2022-12-14 10:35:56 -0500

    react structure take 2

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Development from './containers/Development.jsx'
+import Production from './containers/Production.jsx'
+
+
+function App() {
+  return (
+    <div>
+      <Development/>
+      <Production/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/containers/Production.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//import components here
+import URIList from '../components/URIList.jsx'
+import Charts from '../components/Charts.jsx';
+
+const Production = () => {
+  return (
+    <div className='productionContainer'>
+      <h1>HEY WORLD</h1>
+      {/* <URIList /> */}
+      {/* <Charts/> */}
+    </div>
+  )
+}
+
+export default Production;
