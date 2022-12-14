c9c9a5073250270c51d1d9807c9b477f35360829
Jonathan H <jonhuang5@gmail.com>
2022-12-14 12:42:09 -0500

    Prometheus server scraping; new back-end routes to pass along metrics + URIs (#5)
    * Added dependencies; new route to get metrics from prom-server
    
    * Set up route to scrape Prometheus metrics; parse metrics
    
    * Added routes to start/end monitoring; scrapes data from Prom server and converts to json
    
    * Removed unused dependencies
    
    * Created new route to provide list of endpoints in user's server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,52 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-require("dotenv").config();
+const fetch = require("node-fetch");
+const prom2Json = require("prom2json-se");
 
 const MODE = process.env.NODE_ENV || "production";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9990;
 
 app.use(express.json());
 
 if (MODE === "production") {
-  app.use("/", express.static(path.join(__dirname, "../dist")));
+  app.use(express.static(path.join(__dirname, "../dist")));
 }
+
+let intervalId;
+let promData;
+
+const fetchDataFromPromServer = async (req, res) => {
+  try {
+    const promServerResponse = await fetch("http://localhost:9991/metrics");
+    const { status } = promServerResponse;
+    console.log(new Date().toUTCString(), status);
+    promData = prom2Json.convert(await promServerResponse.text());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+app.get("/routes", (req, res) => {
+  res.status(200).json(["route1", "route2", "route3", "route4"]);
+})
+
+app.post("/monitoring", (req, res) => {
+  const { active, interval } = req.body; // active is a boolean, interval is in seconds
+  if (active)
+    intervalId = setInterval(fetchDataFromPromServer, interval * 1000);
+  else clearInterval(intervalId);
+  console.log("ACTIVE:", active);
+  res.sendStatus(200);
+});
+
+app.get("/metrics", async (req, res) => {
+  return res.status(200).json(promData);
+});
 
 app.listen(PORT, () => {
   console.log(
-    `Express server listening on port ${PORT}\n${MODE.toUpperCase()} mode`
+    `Application server started on port ${PORT}\n${MODE.toUpperCase()} mode`
   );
 });
