df320733a294f3eb3e2351109b93792d65fc133b
JH51 <jonhuang5@gmail.com>
2022-12-15 17:00:05 -0500

    Reconfigure webpack to serve static assets

--- electron/electron-main.js
+++ electron/electron-main.js
@@ -1,32 +1,33 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+const fs = require("fs");
+require("dotenv").config();
 
+const { SERVER_URL } = process.env;
 
 console.log("ELECTRON RUNNING SMOOTH");
 
 function createWindow() {
-
   let win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-
-  })
-
-  win.loadFile("./dist/index.html")
-
+      nodeIntegration: true,
+    },
+  });
+  console.log(fs.readdirSync("./dist"));
+  win.loadFile("./dist/index.html");
+  // win.loadURL("http://localhost:8080")
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
