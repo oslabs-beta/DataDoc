805bdfc0cdbcdce8cca44fb3f5fcd9b9b657b102
Jo Huang <johuang@Jos-MacBook-Air.local>
2022-12-14 20:30:33 -0500

    additional files

--- client/components/Histogram.jsx
+++ client/components/Histogram.jsx
@@ -1,51 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { faker } from '@faker-js/faker';
 import dataObj from './data.js'
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+const Histogram = (props) => {
 
-console.log(dataObj[0].values)
+  const [histData, setHistData] = useState({})
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  useEffect(() => {
+    fetch('http://localhost:9990/histogram')
+      .then(res => res.json())
+      .then(test => setHistData(test))
+  }, [])
+  
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Response Time Distribution',
+      },
     },
-    title: {
-      display: true,
-      text: 'Response Time Distribution',
-    },
-  },
-};
-
-const labels = ['0', '0.5', '1', '1.5', '2'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    // {
-    //   label: 'Dataset 2',
-    //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    // },
-  ],
-};
-
-export default function Histogram() {
-  return <Bar options={options} data={data} />;
+  };
+  console.log(histData)
+  const data = {
+    labels: Object.keys(histData).sort((a, b) => a - b ),
+    datasets: [
+      {
+        label: 'Response Time (ms)',
+        data: Object.values(histData),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(255, 99, 132, 0.5)'        
+        ]
+      },
+    ],
+  };
+  
+  return (
+    <Bar 
+      data={data} 
+      options={options} 
+    />
+  )
 }
+
+export default Histogram;
