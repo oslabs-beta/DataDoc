40a32c2cfefa04cdb7098454703cdafd5d470a8d
Jamie Schiff <jamie.abrams.schiff@gmail.com>
2022-12-15 10:32:26 -0500

    jamie and maria reconcile

--- client/components/URI.jsx
+++ client/components/URI.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
 const URI=(props)=>{
+  const {endpoint, status} = props
+
   return(
     <div>
-      <button>Delete</button>
-      
+      <section className="tableHeader">
+        <title>Available URI Endpoints</title>
+        <table>
+          <thead>
+            <tr>
+              <th>Tracking</th>
+              <th>URI Endpoint</th>
+              <th>Status Code</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+            <span>
+        <input type="checkbox" id="checked" name="checkTrackingURI" checked="checked"></input>
+        <field>{endpoint}</field>
+        <field>{status}</field>
+      </span>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </div>
   )
 }
--- client/components/URIList.jsx
+++ client/components/URIList.jsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from 'react';
 //generates unique keys
 import { v4 as uuidv4 } from 'uuid';
-import testData from './dummydata'
 
 //import other components here
 import URI from './URI.jsx'
 import FlashError from './FlashError.jsx';
 
-const test = [
-  { endpoint: "route1", status: 200 },
-  { endpoint: "route2", status: 200 },
-  { endpoint: "route3", status: 200 },
-  { endpoint: "route4", status: 200 }
-]
 
 const URIList=(props)=>{
   const [URIList, setURIList] = useState([])
@@ -20,25 +13,21 @@ const URIList=(props)=>{
 
   //fetch the URI List from the backend when the component mounts
   useEffect(()=> {
-    fetch('http://localhost:9990/routes')
+    fetch(`http://localhost:${process.env.PORT}/routes`)
      .then(response=>response.json())
      .then(data=>{
       setURIList(()=> data)
       }).catch((err)=>{
         console.log(err)
-        // setErrorMessage('Invalid fetch request for sthe URI List.')
+        setErrorMessage('Invalid fetch request for the URI List')
         // reset the error message
-        // setTimeout(()=> setErrorMessage(''), 3000)
+        setTimeout(()=> setErrorMessage(''), 5000)
       })
-      // setURIList(test.map((element)=>{
-      //   return <URI key={uuidv4()} endpoint={element.endpoint} status={element.status}/>
-      // }))
-      // console.log(URIList)
   },[])
   return(
     <div className='URIListContainer'>
       <div className='URIEntries'>
-        {/* {errorMessage !== '' ? <FlashError errorMessage={errorMessage}/> : null} */}
+        {errorMessage !== '' ? <FlashError errorMessage={errorMessage}/> : null}
         {URIList.map((element)=>{
           return <URI key={uuidv4()} endpoint={element.endpoint} status={element.status}/>
         })}
