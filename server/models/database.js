bb58a3d0327c10eb0864dc6e471bf114a41926bd
Jo Huang <johuang@Jos-MacBook-Air.local>
2022-12-19 19:43:26 -0500

    saving progress

--- server/models/database.js
+++ server/models/database.js
@@ -3,17 +3,21 @@ const dotenv = require('dotenv')
 const path = require('path')
 dotenv.config({path: path.resolve(__dirname, "../../.env")});
 
-const ADMIN_TOKEN = process.env.DB_INFLUXDB_INIT_ADMIN_TOKEN
-const ORG = process.env.DB_INFLUXDB_INIT_ORG
-const BUCKET = process.env.DB_INFLUXDB_INIT_BUCKET
+const token = process.env.DB_INFLUXDB_INIT_ADMIN_TOKEN
+const org = process.env.DB_INFLUXDB_INIT_ORG
+const bucket = process.env.DB_INFLUXDB_INIT_BUCKET
 
-// creates a new instance of influxDB
-const client = new InfluxDB({url: 'http://localhost:8086', token: ADMIN_TOKEN})
+// create a new instance of influxDB, providing URL and API token
+const client = new InfluxDB({url: 'http://localhost:8086', token: token})
+// create a write client, providing influxDB organization and bucket name
+const writeApi = client.getWriteApi(org, bucket, 's')
+// create default tags to all points
+writeApi.useDefaultTags({endpoint: '/signup'})
+
 // 
-const writeApi = client.getWriteApi(ORG, BUCKET, 's')
-const point = new Point('mem').floatField('used_percent', 50)
+const point = new Point('mem')
+    .floatField('used_percent', 50)
 
-writeApi.useDefaultTags({host: 'host1'})
 writeApi.writePoint(point)
 
 writeApi
