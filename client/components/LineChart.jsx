6cd74b29e4cb11f440799649a531d3dc960e2987
JH51 <jonhuang5@gmail.com>
2022-12-21 10:46:28 -0500

    Start of time-series

--- client/components/LineChart.jsx
+++ client/components/LineChart.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
+  TimeScale,
   LinearScale,
   PointElement,
   LineElement,
@@ -19,7 +20,8 @@ const LineChart = (props) => {
   const [lineData, setLineData] = useState([]);
 
   ChartJS.register(
-    CategoryScale,
+    // CategoryScale,
+    TimeScale,
     LinearScale,
     PointElement,
     LineElement,
@@ -32,32 +34,58 @@ const LineChart = (props) => {
     fetch(`${SERVER_URL}/chartdata/linechart/${id}`)
       .then((serverResponse) => serverResponse.json())
       .then((serverResponseJson) => {
-        const {labels, data } = serverResponseJson.respTimeLineData;
-        setLineLabels(labels);
+        const { labels, data } = serverResponseJson.respTimeLineData;
+        console.log(labels.map(e => new Date(e).getTime()));
+        setLineLabels(labels.map(e => new Date(e).getTime()));
         setLineData(data);
       });
   }, []);
 
+  const values = [
+    {
+      x: new Date("2020-01-01"),
+      y: 100.2
+    },
+    {
+      x: new Date("2020-01-02"),
+      y: 102.2
+    },
+    {
+      x: new Date("2020-01-03"),
+      y: 105.3
+    },
+    {
+      x: new Date("2020-01-11"),
+      y: 104.4
+    }
+  ];
+
   const data = {
-    labels: lineLabels,
+    // labels: lineLabels,
     datasets: [
       {
-        label: "My First Dataset",
-        data: lineData,
+        // label: "My First Dataset",
+        data: values,
         fill: false,
         borderColor: [
-          "rgb(75, 192, 192)",
-          "rgb(75, 192, 192)",
-          "rgb(75, 192, 192)",
-          "rgb(75, 192, 192)",
-          "rgb(75, 192, 192)",
-          "rgb(75, 192, 192)",
           "rgb(75, 192, 192)",
         ],
         tension: 0.1,
       },
     ],
   };
+
+  const options = {
+    response: true,
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "day"
+        }
+      }
+    }
+  }
 
   return (
     <div className="line-chart">
