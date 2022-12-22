a984a9fc4a205d250ff90116fff9d88efbc26bce
jochuang <joh0324@gmail.com>
2022-12-22 11:27:21 -0500

    Integrate querying data from database with automated pinging of endpoints and storing metrics in database (#33)
    * added query functionality in the getHistogramData middleware function
    
    * integrating automated pinging of endpoints and storing data in database with querying data from database
    
    Co-authored-by: Jo Huang <johuang@Jos-MacBook-Air.local>

--- server/routes/chartdata.js
+++ server/routes/chartdata.js
@@ -5,10 +5,11 @@ const dbController = require('../controllers/dbController');
 // get line chart data
 router.get('/linechart/:id',
     dbController.getRespTimeLineData,
-    // dbController.getRespTimeHistData,
-    // dbController.getReqFreqLineData,
-    // dbController.getStatusPieData,
+    dbController.getRespTimeHistData,
+    dbController.getReqFreqLineData,
+    dbController.getStatusPieData,
     (req, res) => {
+        console.log('this is res.locals.data', res.locals.data)
         return res.status(200).json(res.locals.data);
     });
 
