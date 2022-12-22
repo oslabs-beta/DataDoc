bf7463fbeeb19dfd8060886a2f4d18a45e3c6efd
mariamzakariadze <mariazakariadze@gmail.com>
2022-12-22 11:44:12 -0500

    Added sliders with state

--- client/containers/Development.jsx
+++ client/containers/Development.jsx
@@ -1,15 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Slider } from '@mui/material';
 
 
 const Development=(props)=>{
   const {setSimulation} = props
+  const [RPM, setRPM] = useState(0)
+  const [timeInterval, setTimeInterval] = useState(0)
+  
   useEffect(()=>{
     setSimulation(true)
   }, [])
+
+  function valuetext(value) {
+		return `${value}`;
+	}
+
+  const updateRPM = (e, data) => {
+    setRPM(data);
+  };
+
+  const updateInterval = (e, data) => {
+    setTimeInterval(data);
+  };
+
+  console.log("THIS IS RPM", RPM)
+  console.log("THIS IS INTERVAL", timeInterval)
+
   return(
     <div className='developmentContainer'>
       <h1>HELLO WORLD</h1>
-    </div>
+      <Box display="flex" flexDirection="column" m={10}>
+      <Slider
+        aria-label="Temperature"
+        defaultValue={30}
+        getAriaValueText={valuetext}
+        valueLabelDisplay="auto"
+        step={100}
+        style={{ width: 500, marginLeft: 300, marginTop: 100}}
+        marks
+        min={10}
+        max={1000}
+        value = {RPM}
+        onChange ={updateRPM}
+          />
+  
+        <Slider
+        aria-label="Temperature"
+        defaultValue={30}
+        getAriaValueText={valuetext}
+        style={{ width: 500, marginLeft: 300}}
+        valueLabelDisplay="auto"
+        step={1}
+        marks
+        min={1}
+        max={20}
+        value = {timeInterval}
+        onChange ={updateInterval}
+          />
+      </Box>
+      </div>
   )
 }
 
