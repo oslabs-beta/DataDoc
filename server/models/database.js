278b10ae9600b3cb0ff04d2e11be74ddbd249e9d
JH51 <jonhuang5@gmail.com>
2023-01-05 14:25:10 -0500

    Add options object to InfluxDB Client; compress DB writes with gzip

--- server/models/database.js
+++ server/models/database.js
@@ -1,39 +1,48 @@
-const { InfluxDB, Point } = require('@influxdata/influxdb-client');
-const dotenv = require('dotenv')
-const path = require('path');
-dotenv.config({path: path.resolve(__dirname, "../../.env")});
+const { InfluxDB, Point } = require("@influxdata/influxdb-client");
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-const token = process.env.DB_INFLUXDB_INIT_ADMIN_TOKEN
-const org = process.env.DB_INFLUXDB_INIT_ORG
-const bucket = process.env.DB_INFLUXDB_INIT_BUCKET
+const token = process.env.DB_INFLUXDB_INIT_ADMIN_TOKEN;
+const org = process.env.DB_INFLUXDB_INIT_ORG;
+const bucket = process.env.DB_INFLUXDB_INIT_BUCKET;
 
 const insertToDB = () => {
-    // create a new instance of influxDB, providing URL and API token
-    const client = new InfluxDB({url: 'http://localhost:8086', token: token})
-    // create a write client, providing influxDB organization and bucket name
-    const writeApi = client.getWriteApi(org, bucket, 'ns')
-    // create default tags to all points
-    // writeApi.useDefaultTags({endpoint: '/signup'})
+  // create a new instance of influxDB, providing URL and API token
+  const client = new InfluxDB({ url: "http://localhost:8086", token: token });
+  // create a write client, providing influxDB organization and bucket name
+  const writeApi = client.getWriteApi(org, bucket, "ns");
+  // create default tags to all points
+  // writeApi.useDefaultTags({endpoint: '/signup'})
 
-    // use the point constructor passing in "measurement" (table)
-    const point = new Point('metrics')
-        .tag('path','/good')
-        .tag('method','GET')
-        .floatField('res_time', 60)
-        .intField('status_code', 200)
-        // .timestamp()
-        
-    writeApi.writePoint(point)
+  // use the point constructor passing in "measurement" (table)
+  const point = new Point("metrics")
+    .tag("path", "/good")
+    .tag("method", "GET")
+    .floatField("res_time", 60)
+    .intField("status_code", 200);
+  // .timestamp()
 
-    writeApi.close().then(() => {
-        console.log('WRITE FINISHED')
-    })
-}
+  writeApi.writePoint(point);
+
+  writeApi.close().then(() => {
+    console.log("WRITE FINISHED");
+  });
+};
 
 const insertMultiple = (pointsArr) => {
   try {
-    const client = new InfluxDB({ url: "http://localhost:8086", token: token });
-    const writeApi = client.getWriteApi(org, bucket, "ms");
+    const client = new InfluxDB({
+      url: "http://localhost:8086",
+      token: token,
+      options: {
+        headers: { "Content-Encoding": "gzip" }
+      }
+    });
+    const writeApi = client.getWriteApi(org, bucket, "ms", {
+      gzipThreshold: 0,
+      headers: { "Content-Encoding": "gzip" }
+    });
     writeApi.writePoints(pointsArr);
     writeApi.close();
     return true;
@@ -43,13 +52,13 @@ const insertMultiple = (pointsArr) => {
 };
 
 const insertRegistration = (point) => {
-  const client = new InfluxDB({url: 'http://localhost:8086', token: token})
-  const writeApi = client.getWriteApi(org, bucket, 'ns')
-      
-  writeApi.writePoint(point)
+  const client = new InfluxDB({ url: "http://localhost:8086", token: token });
+  const writeApi = client.getWriteApi(org, bucket, "ns");
+
+  writeApi.writePoint(point);
   writeApi.close().then(() => {
-      console.log('WRITE FINISHED')
-  })
-}
+    console.log("WRITE FINISHED");
+  });
+};
 
 module.exports = { insertToDB, insertMultiple, insertRegistration };
