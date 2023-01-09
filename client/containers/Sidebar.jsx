ec82103e0302fdb0dbba10f93a0d1d9dda58e13d
Jo Huang <joh0324@gmail.com>
2023-01-08 20:25:57 -0500

    sidebar not responding to css changes

--- client/containers/Sidebar.jsx
+++ client/containers/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 // import "react-pro-sidebar/dist/css/styles.css";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 // import { Link } from "react-router-dom";
@@ -78,33 +78,13 @@ const Sidebar = () => {
                 </IconButton>
             )}
           </MenuItem>
+          <MenuItem>
+            WorkSpace1
+          </MenuItem>
+          <MenuItem>
+            WorkSpace2
+          </MenuItem>
 
-          {!isCollapsed && (
-            <Box mb="25px">
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <img
-                  alt="profile-user"
-                  width="100px"
-                  height="100px"
-                  src={""}
-                  style={{ cursor: "pointer", borderRadius: "50%" }}
-                />
-              </Box>
-              <Box textAlign="center">
-                <Typography
-                  variant="h2"
-                  color={colors.grey[100]}
-                  fontWeight="bold"
-                  sx={{ m: "10px 0 0 0" }}
-                >
-                  The Best CEO
-                </Typography>
-                <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
-                </Typography>
-              </Box>
-            </Box>
-          )}
           <Box paddingLeft={isCollapsed ? undefined : "10%"}></Box>
         </Menu>
         </div>
