78fc51e20e8e98a33872776b47c8026e2adf3cec
JH51 <jonhuang5@gmail.com>
2023-01-10 14:46:11 -0500

    DrawerContents; add navigate() to ListItemButton

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -9,6 +9,7 @@ import URIList from "../components/URIList.jsx";
 import Workspace from "../components/Workspace.jsx";
 import Dashboard from "./Dashboard.jsx";
 import Development from "./Development.jsx";
+import DrawerContents from "./DrawerContents.jsx";
 // import NavBar from "./NavBar.jsx";
 import SideBar from "./NavBar.jsx";
 import TopBar from "./TopBar.jsx";
@@ -136,39 +137,7 @@ const App = () => {
                 </IconButton>
               </DrawerSection>
               <Divider />
-              <List>
-                {["Inbox", "Starred", "Send email", "Drafts"].map(
-                  (text, index) => (
-                    <ListItem
-                      key={text}
-                      disablePadding
-                      sx={{ display: "block" }}
-                    >
-                      <ListItemButton
-                        sx={{
-                          minHeight: 48,
-                          justifyContent: open ? "initial" : "center",
-                          px: 2.5
-                        }}
-                      >
-                        <ListItemIcon
-                          sx={{
-                            minWidth: 0,
-                            mr: open ? 3 : "auto",
-                            justifyContent: "center"
-                          }}
-                        >
-                          <Menu />
-                        </ListItemIcon>
-                        <ListItemText
-                          primary={text}
-                          sx={{ opacity: open ? 1 : 0 }}
-                        />
-                      </ListItemButton>
-                    </ListItem>
-                  )
-                )}
-              </List>
+              <DrawerContents open={open} />
             </Drawer>
             {/* <SideBar open={open} theme={theme} handledrawerclose={handleDrawerClose} /> */}
 
@@ -196,6 +165,7 @@ const App = () => {
             </Box>
 
           </Router>
+          
         </Box>
       </ThemeProvider>
     </ColorModeContext.Provider>
---
+++ client/containers/DrawerContents.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+const DrawerContents = (props) => {
+  const { open } = props;
+  const [workspaceList, setWorkspaceList] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getWorkSpaceList();
+    return;
+  }, []);
+
+  const getWorkSpaceList = async () => {
+    const newWorkspaceList = await (
+      await fetch(`http://localhost:${process.env.PORT}/workspaces`)
+    ).json();
+    setWorkspaceList(newWorkspaceList);
+    return;
+  };
+
+  return (
+    <List>
+      {workspaceList.map((workspace) => {
+        return (
+          <ListItem
+            key={crypto.randomUUID()}
+            disablePadding
+            sx={{ display: "block" }}
+          >
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5
+              }}
+              onClick={() => {
+                navigate(`urilist/${workspace._id}`, {
+                  state: {
+                    workspace_id: workspace._id,
+                    name: workspace.name,
+                    domain: workspace.domain
+                  }
+                });
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center"
+                }}
+              >
+                <Menu />
+              </ListItemIcon>
+              <ListItemText
+                primary={workspace.name}
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+
+export default DrawerContents;
